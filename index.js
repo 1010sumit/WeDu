02afb6609cd2e99205c2aca7dababee0f6c8c588
Your Name <your.email@example.com>
2024-01-22 18:47:02 +0530

    WeDu

---
+++ index.js
@@ -0,0 +1,10 @@
+let menutoggle = document.querySelector('.toggle-btn');
+menutoggle.addEventListener('click',function(){
+    menutoggle.classList.toggle('active')
+})
+
+// function scroll(){
+//     var baneffect = querySelector('.ba');
+//     var position = baneffect.getBoundingClientRect().top;
+// }
+// scroll();
